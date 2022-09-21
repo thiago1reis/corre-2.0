0ddda2093fc19346ef770a029f4c6148a5a9fcce
thiago1reis <thiagoalexandreis@gmail.com>
2022-09-21 18:14:50 -0300

    Melhorias no cards do painel admin

--- resources/views/livewire/painel.blade.php
+++ resources/views/livewire/painel.blade.php
@@ -1,34 +1,33 @@
 <div class="container">
-    <div class="row row-cols-sm-4 g-4 pt-3 text-secondary mb-3">
-        <div class="col-sm-6 col-md-6 col-lg-4">
-            <div class="card h-100 border-secondary mb-3" >
-                <div class="card-header bg-transparent border-secondary"> <h4 class="text-uppercase"><i class="icofont-ui-add"></i>  Adicionar</h4></div>
-                <div class="card-body ms-4">
-                    <p class="card-text "><i class="icofont-ui-play"></i> <a href="{{ route('alunos') }}" class="card-link text-secondary">Alunos</a></p>
-                    <p class="card-text "><i class="icofont-ui-play"></i> <a href="{{ route('disciplinas') }}" class="card-link text-secondary">Disciplinas</a></p>
-                    <p class="card-text "><i class="icofont-ui-play"></i> <a href="#" class="card-link text-secondary">Servidores</a></p>
-                    <p class="card-text "><i class="icofont-ui-play"></i> <a href="#" class="card-link text-secondary">Turmas</a></p>
-                    <p class="card-text "><i class="icofont-ui-play"></i> <a href="#" class="card-link text-secondary">Usuários</a></p>
+    <div class="row text-secondary my-3">
+        <div class="col-sm-6 col-md-6 col-lg-4 d-flex justify-content-center">
+            <div class="card card-painel border-secondary mb-3">
+                <div class="card-body mx-4 my-2">
+                    <h4 class="text-uppercase mb-4"><i class="icofont-ui-add"></i> Adicionar</h4>
+                    <a href="{{ route('alunos') }}" class="nav-link"> Alunos</a>
+                    <a href="{{ route('disciplinas') }}" class="nav-link"> Disciplinas</a>
+                    <a href="#" class="nav-link"> Servidores</a>
+                    <a href="#" class="nav-link"> Turmas</a>
+                    <a href="#" class="nav-link"> Usuários</a>
                 </div>  
             </div>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-4">
-            <div class="card h-100 border-secondary mb-3" >
-                <div class="card-header bg-transparent border-secondary"> <h4 class="text-uppercase"><i class="icofont-ui-note"></i>  Cadastrar</h4></div>
-                <div class="card-body ms-4">
-                    <p class="card-text "><i class="icofont-ui-play"></i> <a href="#" class="card-link text-secondary">Ocorrências</a></p>
-                </div>
+        <div class="col-sm-6 col-md-6 col-lg-4 d-flex justify-content-center">
+            <div class="card card-painel border-secondary mb-3">
+                <div class="card-body mx-4 my-2">
+                    <h4 class="text-uppercase mb-4"><i class="icofont-ui-note"></i> Cadastrar</h4>
+                    <a href="#" class="nav-link">Ocorrências</a>
+                </div>  
             </div>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-4">
-            <div class="card h-100 border-secondary mb-3" >
-                <div class="card-header bg-transparent border-secondary"> <h4 class="text-uppercase"><i class="icofont-ui-search"></i>  Consultar</h4></div>
-                    <div class="card-body ms-4">
-                        <p class="card-text "><i class="icofont-ui-play"></i> <a href="#" class="card-link text-secondary">Alunos</a></p>
-                        <p class="card-text "><i class="icofont-ui-play"></i> <a href="#" class="card-link text-secondary">Ocorrências</a></p>
-                    </div>
-                </div>
-            </div> 
+        <div class="col-sm-6 col-md-6 col-lg-4 d-flex justify-content-center">
+            <div class="card card-painel border-secondary mb-3">
+                <div class="card-body mx-4 my-2">
+                    <h4 class="text-uppercase mb-4"><i class="icofont-ui-search"></i> Consultar</h4>
+                    <a href="#" class="nav-link">Alunos</a>
+                    <a href="#" class="nav-link">Ocorrências</a>
+                </div>  
+            </div>
         </div>
     </div>
 </div>    
