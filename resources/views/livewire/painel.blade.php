e645f3839772a7c81e12b35bc4184beb70f283af
thiago1reis <thiagoalexandreis@gmail.com>
2022-11-12 20:00:28 -0300

    Altera cards do painel admin

--- resources/views/livewire/painel.blade.php
+++ resources/views/livewire/painel.blade.php
@@ -1,33 +1,104 @@
-<div class="container">
-    <div class="row text-secondary my-3">
-        <div class="col-sm-6 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card card-painel border-secondary mb-3">
-                <div class="card-body mx-4 my-2">
-                    <h4 class="text-uppercase mb-4"><i class="icofont-ui-add"></i> Adicionar</h4>
-                    <a href="{{ route('alunos') }}" class="nav-link"> Alunos</a>
-                    <a href="{{ route('disciplinas') }}" class="nav-link"> Disciplinas</a>
-                    <a href="#" class="nav-link"> Servidores</a>
-                    <a href="#" class="nav-link"> Turmas</a>
-                    <a href="#" class="nav-link"> Usuários</a>
-                </div>  
-            </div>
+<div class="container-fluid">
+    <div class="row mt-3" > 
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-add icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Adicionar Alunos</h6>
+                    </div>
+                </div>
+            </a>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card card-painel border-secondary mb-3">
-                <div class="card-body mx-4 my-2">
-                    <h4 class="text-uppercase mb-4"><i class="icofont-ui-note"></i> Cadastrar</h4>
-                    <a href="#" class="nav-link">Ocorrências</a>
-                </div>  
-            </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-add icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Adicionar Disciplinas</h6>
+                    </div>
+                </div>
+            </a>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card card-painel border-secondary mb-3">
-                <div class="card-body mx-4 my-2">
-                    <h4 class="text-uppercase mb-4"><i class="icofont-ui-search"></i> Consultar</h4>
-                    <a href="#" class="nav-link">Alunos</a>
-                    <a href="#" class="nav-link">Ocorrências</a>
-                </div>  
-            </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-add icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Adicionar Servidores</h6>
+                    </div>
+                </div>
+            </a>
         </div>
-    </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-add icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Adicionar Turmas</h6>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-add icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Adicionar Usuários</h6>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-note icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Cadastrar Ocorrências</h6>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-search icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Consultar Aluno</h6>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-search icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Consultar Ocorrências</h6>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-settings icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Config. Usuário</h6>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+            <a href="{{ route('logout') }}" class="nav-link admin-option">
+                <div class="card bg-white text-bg-light mb-3 py-4 shadow-sm " >
+                    <div class="card-body text-center text-secondary">
+                        <i class="icofont-ui-close icofont-3x"></i>
+                        <h6 class="card-title fw-light mt-3">Sair</h6>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div> 
 </div>    
