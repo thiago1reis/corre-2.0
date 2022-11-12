73adb076221d5f7a43f9807e46a3dc8f8db209c9
thiago1reis <thiagoalexandreis@gmail.com>
2022-11-12 18:30:58 -0300

    Altera opções no menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,10 +4,13 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+       
         <li class="nav-item px-3">
           <a class="nav-link" aria-current="page" href="{{ route('painel') }}"><i class="icofont-ui-home"></i> Início</a>
         </li>
+        
         <li class="nav-item dropdown px-3">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             <i class="icofont-ui-add"></i> 
@@ -18,13 +21,24 @@
             <li><a class="dropdown-item" href="{{ route('disciplinas') }}">Disciplinas</a></li>
           </ul>
         </li>
+
         <li class="nav-item px-3">
           <a class="nav-link" href="#"><i class="icofont-ui-note"></i> Cadastar Ocorrência</a>
         </li>
-        <li class="nav-item px-3">
-          <a class="nav-link" href="#"><i class="icofont-ui-search"></i> Consultar Aluno</a>
+
+        <li class="nav-item dropdown px-3">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="icofont-ui-search"></i> 
+            Consultar
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="">Aluno</a></li>
+            <li><a class="dropdown-item" href="">Ocorrencias</a></li>
+          </ul>
         </li>
+
       </ul>
+
       <div class="d-flex">
         <ul class="navbar-nav">
           <li class="nav-item dropdown px-3">
