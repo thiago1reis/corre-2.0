2c74ce5e1ee919ccf1e9146606d0d447f6d688d1
thiago1reis <thiagoalexandreis@gmail.com>
2023-04-18 23:34:59 -0300

    Altera cor de fundo da modal de salvar turmas

--- resources/views/turma/modal-salvar-turma.blade.php
+++ resources/views/turma/modal-salvar-turma.blade.php
@@ -1,5 +1,5 @@
 <div class="modal-dialog modal-xl">
-    <div class="modal-content">
+    <div class="modal-content bg-modal">
         <div class="modal-header">
             <h5 class="modal-title">{{ $acao }}</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
