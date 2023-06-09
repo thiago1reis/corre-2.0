1faa0a2d2a6775064e097c6be4f28dbea70a415d
thiago1reis <thiagoalexandreis@gmail.com>
2023-06-09 11:48:33 -0300

    Altera modo que os números de telefones são gerados

--- database/factories/AlunoFactory.php
+++ database/factories/AlunoFactory.php
@@ -21,10 +21,10 @@ class AlunoFactory extends Factory
             'matricula' => $this->faker->unique()->numerify(str_repeat('#', 14)),
             'data_nascimento' => $this->faker->dateTimeInInterval($date = '-15 years', $interval = '+5 days'),
             'sexo' => $this->faker->randomElement(['Masculino', 'Feminino', 'Outros']),
-            'telefone' => $this->faker->unique()->phoneNumber(),
+            'telefone' => $this->faker->randomElement(['(00) 0 0000-0000', null]),
             'email' => $this->faker->unique()->safeEmail(),
             'responsavel' => $this->faker->randomElement([$this->faker->name(), '']),
-            'telefone_responsavel' => $this->faker->randomElement([$this->faker->unique()->phoneNumber(), '']),
+            'telefone_responsavel' => $this->faker->randomElement(['(00) 0 0000-0000', null]),
             'observacao' => $this->faker->text($maxNbChars = 30)
         ];
     }
